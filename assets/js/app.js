861e29744f5b0a6d0b313c518813301a7a842137
thomas cote <tcote19@gmail.com>
2017-12-02 17:07:46 -0800

    Updated index, Js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,32 @@
-var gifs = [];
 
-    $("button").on("click", function() {
+var topics = ["Superbikes", "Supercars", "Jets", "Planes", "Trains", "Trucks", "Gocarts"];
+var addedTopics = [];
+
+
+	var getButtons = function(){
+        for (var i = 0; i < topics.length; i++){
+        	var newBtn = $("<button data-vehicle" + "=" + topics[i] + ">" + topics[i] + "</button>");
+
+        	$("#buttons").append(newBtn);
+        };
+    };
+    getButtons();
+
+    var newButtons = function(){
+    	for (var o = 0; o < addedTopics.length; o++){
+        	var newerBtn = $("<button data-vehicle" + "=" + addedTopics[o] + ">" + addedTopics[o] + "</button>");
+
+        	$("#buttons").append(newerBtn);
+        };
+
+        topics += addedTopics;
+    };
+
+    $("#buttons").on("click", function(event) {
+
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + event.target.dataset.vehicle + "&api_key=h4oStqFYKrToifPOOgZ7nWIktOVExtdJ&limit=10";
       var vehicle = $(this).attr("data-vehicle");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        vehicle + "&api_key=h4oStqFYKrToifPOOgZ7nWIktOVExtdJ&limit=10";
-
+      
       $.ajax({
           url: queryURL,
           method: "GET"
@@ -13,7 +35,7 @@ var gifs = [];
           var results = response.data;
 
           for (var i = 0; i < results.length; i++) {
-            var gifyDiv = $("<div class='item'>");
+            var gifyDiv = $("<div>");
 
             var rating = results[i].rating;
 
@@ -28,4 +50,25 @@ var gifs = [];
             $("#giphy-images").prepend(gifyDiv);
           }
         });
-    });
+       });
+
+ $("#add-gif").on("click", function(){
+ 	event.preventDefault();
+ 
+ 	var newInput = $("#gif-input").val();
+
+ 	addedTopics.push(newInput);
+
+ 	newButtons();
+ 	
+ });
+
+
+
+
+
+
+
+
+
+
